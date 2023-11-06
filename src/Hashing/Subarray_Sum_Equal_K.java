756f51f12819db72a44b81f1f5e867f4bb3aa72e
DipRabadiya27 <diprabadiya@gmail.com>
2023-11-06 15:08:59 +0530

    Add Tree

--- src/Hashing/Subarray_Sum_Equal_K.java
+++ src/Hashing/Subarray_Sum_Equal_K.java
@@ -1,8 +1,29 @@
 package Hashing;
 
+import java.util.HashMap;
+
 public class Subarray_Sum_Equal_K {
     public static void main(String[] args) {
-        int[] arr = {1,2,3};
-        int k = 3;
+        int[] arr = {10, 2, -2, -20, 10};
+        int k = -22;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        map.put(0,1);
+        int ans = 0;
+        int sum = 0;
+        for(int j=0;j<arr.length;j++) {
+            sum += arr[j];
+
+            if(map.containsKey(sum-k)){
+                ans += map.get(sum-k);
+            }
+
+            if(map.containsKey(sum)) {
+                map.put(sum, map.get(sum)+1);
+            } else {
+                map.put(sum, 1);
+            }
+        }
+        System.out.println(ans);
     }
 }
